218fcf481ebdc119e8181b0cba81bdb19f654912
Jesse Hallett <jesse@sitr.us>
2021-04-25 13:39:39 -0400

    rearrange keys in qwerty layer to map correctly when OS is set to dvorak

--- keyboards/kinesis/kint36/keymaps/hallettj/keymap.c
+++ keyboards/kinesis/kint36/keymaps/hallettj/keymap.c
@@ -186,10 +186,12 @@ enum my_keycodes {QWERTY = SAFE_RANGE, DVORAK, WIN, MAC, PC, STATUS, PROGRAM};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
+// Keys here are scrambled a little so that they map the way I want them when
+// the OS is in Dvorak mode
 [_QWERTY] = LAYOUT (
            // Left Hand
            KC_ESC,      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,        KC_F8,
-           KC_EQL,      KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
+           KC_RBRC,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
            KC_TAB,      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,
            MO(_KEYPAD), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
            KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,
@@ -202,10 +204,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
            // Right Hand
            KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, KC_SLCK, KC_PAUS, TG(_KEYPAD),  MO(_PROGRAM),
            KC_6,    KC_8,    KC_9,    KC_0,    KC_7,    KC_MINS,
-           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
            KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                    KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC,
+                    KC_UP,   KC_DOWN, KC_MINS, KC_EQL,
            // Right Thumb
            KC_RGUI, KC_BSPC,
            KC_PGUP,
@@ -215,7 +217,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_DVORAK] = LAYOUT (
            // Left Hand
            _______, _______, _______, _______, _______, _______, _______, _______, _______,
-           _______, _______, _______, _______, _______, _______,
+           KC_EQL,  _______, _______, _______, _______, _______,
            _______, KC_QUOT, KC_COMM,  KC_DOT,    KC_P,    KC_Y,
            _______,    KC_A,    KC_O,    KC_E,    KC_U,    KC_I,
            _______, KC_SCLN,    KC_Q,    KC_J,    KC_K,    KC_X,
@@ -231,7 +233,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
            KC_F,       KC_G,    KC_C,    KC_R,    KC_L, KC_SLSH,
            KC_D,       KC_H,    KC_T,    KC_N,    KC_S, KC_MINS,
            KC_B,       KC_M,    KC_W,    KC_V,    KC_Z, _______,
-                    _______, _______, _______, _______,
+                    _______, _______, KC_LBRC, KC_RBRC,
            // Right Thumb
            _______, _______,
            _______,
